85435e784cf5c873ac47dd0af6591f5f1aceb6cd
Umair Hoda <iqbalumairi@gmail.com>
2023-03-15 14:38:14 -0400

    fixed server url added https

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://playlister-api.onrender.com"],
+    origin: ["https://playlister-api.onrender.com"],
     credentials: true,
   })
 );
