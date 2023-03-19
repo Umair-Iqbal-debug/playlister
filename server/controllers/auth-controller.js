f9e73e4333d7079970b62eb83672db4414c2d52e
Umair Hoda <iqbalumairi@gmail.com>
2023-03-18 23:54:06 -0400

    SameSite with capital s and 'none'

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -75,7 +75,7 @@ loginUser = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "None",
+        SameSite: "none",
       })
       .status(200)
       .json({
